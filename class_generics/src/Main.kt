86a6dd12c959e621e52d73b73e2366336e15bf5e
lnarote777 <lnarote777@iesrafaelalberti.es>
2024-02-28 16:33:29 +0100

    organizacion de código

--- class_generics/src/Main.kt
+++ class_generics/src/Main.kt
@@ -2,6 +2,8 @@
 fun main() {
 
     //********CODIGO DE PRUEBA********
+    println("Código Prueba")
+    println("-------------")
     var numbers = listOf("one", "two", "three", "four")
     var numbersRev = reverse(numbers)
 
@@ -12,6 +14,7 @@ fun main() {
     }
 
     println(numbersRev)
+    println()
 
     //Realiza la misma prueba, pero con listas de otros tipos de datos...
     //por ejemplo una lista que combine valores numéricos Int, Float y Double
@@ -23,13 +26,14 @@ fun main() {
     //¿Por qué es posible usarla si parece que la lista contiene tipos de datos diferentes?
 
     /*
-    La podemos usar porque la funcion reverse y la clase Stack son genericos es decir
-    pueden ser utilizados por cualquier tipo de dato, por eso no hay ningun problema
+    La podemos usar porque la función reverse y la clase Stack son genéricos es decir
+    pueden ser utilizados por cualquier tipo de dato, por eso no hay ningún problema
     al crear una lista con distintos tipos de datos.
      */
 
  // ****************LISTA DE CUALQUIER VALOR NUMÉRICO************************************
-
+    println("Lista de cualquier valor numérico")
+    println("---------------------------------")
     val num = listOf(1.3333333333333333, 2, 3.0, -21, 5.05444444)
     val numRev = reverse(num)
     if (!listOf(5.05444444, -21, 3.0, 2, 1.3333333333333333).equals(numRev)) {
@@ -39,10 +43,13 @@ fun main() {
     }
 
     println(numRev)
+    println()
 
- // ****************LISTA DE CUALQUIER DATA CLASS************************************
+ // ****************LISTA DE UNA DATA CLASS************************************
     data class Book(val title: String, val tomo: Int)
 
+    println("Lista de una data class")
+    println("-----------------------")
     val books = listOf(
         Book("Hellsing (Edicion coleccionista)", 4),
         Book("Dragon Ball", 38),
@@ -59,9 +66,12 @@ fun main() {
     }
 
     println(booksRev)
+    println()
 
  // ****************LISTA DE CUALQUIER TIPO DE DATO************************************
 
+    println("Lista de cualquier tipo de dato")
+    println("-------------------------------")
     val any = listOf(
         "Caballo",
         Book("Kaiju 8", 7),
